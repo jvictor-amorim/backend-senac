4151f9e4d93b787169d5ff0da60715a638ad2cad
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-03-27 09:57:18 -0300

    feat: add courses

---
+++ prisma/courses.ts
@@ -0,0 +1,51 @@
+export const cursos = [
+    {
+        name: 'Administração',
+        description: 'Curso de Bacharelado em Administração',
+        level: 'Graduação',
+        modality: 'Presencial',
+        duration: '4 anos',
+        area: 'Ciências Humanas e Sociais',
+    },
+    {
+        name: 'Técnico em Enfermagem',
+        description: 'Curso Técnico em Enfermagem',
+        level: 'Técnico',
+        modality: 'Presencial',
+        duration: '2 anos',
+        area: 'Saúde',
+    },
+    {
+        name: 'Gastronomia',
+        description: 'Curso de Bacharelado em Gastronomia',
+        level: 'Bacharelado',
+        modality: 'Presencial',
+        duration: '2,5 anos',
+        area: 'Ciências Humanas e Sociais',
+    },
+    {
+        name: 'Desenvolvimento Web - Front End',
+        description: 'Curso profissionalizante em Front-End',
+        level: 'Profissionalizante',
+        modality: 'EAD',
+        duration: '6 meses',
+        area: 'Tecnologia',
+    },
+    {
+        name: 'Desenvolvimento Web - Front End',
+        description: 'Curso profissionalizante em Front-End',
+        level: 'Profissionalizante',
+        modality: 'EAD',
+        duration: '6 meses',
+        area: 'Tecnologia',
+    },
+    {
+        name: 'Desenvolvimento de Jogos Digitais',
+        description: 'Curso de bacharelado em Jogos Digitais',
+        level: 'Bacharelado',
+        modality: 'Híbrido',
+        duration: '2,5 anos',
+        area: 'Tecnologia',
+    },
+
+]
