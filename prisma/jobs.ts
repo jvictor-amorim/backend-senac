ca680ecf8a01a5ad717f0626453fe75815fd03c5
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-04-05 09:30:11 -0300

    perf: ajustes na entidade curso

---
+++ prisma/jobs.ts
@@ -0,0 +1,38 @@
+export const jobs = [ 
+    {
+        enterprise: 'Google',
+        owner: 'Dev Front End Jr NodeJS e ReactJS',
+        quantity: 5,
+        active: true,
+        place: 'Estados Unidos',
+        courseId: '8a63260c-f948-415b-a05e-3714f67e7ace',
+    },
+    {
+        enterprise: 'Accenture',
+        owner: 'Dev Front End Pleno ReactNative',
+        quantity: 2,
+        place: 'Recife',
+        courseId: '8a63260c-f948-415b-a05e-3714f67e7ace',
+    },
+    {
+        enterprise: 'Meta',
+        owner: 'Dev Front End Sr PHP',
+        quantity: 6,
+        place: 'Estados Unidos',
+        courseId: '8a63260c-f948-415b-a05e-3714f67e7ace',
+    },
+    {
+        enterprise: 'Avanade',
+        owner: 'Dev Front End Jr JavaScript',
+        quantity: 1,
+        place: 'São Paulo',
+        courseId: '8a63260c-f948-415b-a05e-3714f67e7ace',
+    },
+    {
+        enterprise: 'Tesla',
+        owner: 'Dev Front End HTML5 e CSS3',
+        quantity: 9,
+        place: 'Canadá',
+        courseId: '8a63260c-f948-415b-a05e-3714f67e7ace',
+    }
+]
