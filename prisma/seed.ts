0f7b706e80a69d61de68a5512d25a7cf4cc46bb4
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-04-05 09:55:22 -0300

    perf: melhoria na modelagem de dados

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { cursos } from './courses';
 import { usuarios } from './users';
+import { jobs } from './jobs';
 
 const prisma = new PrismaClient();
 
@@ -13,15 +14,23 @@ async function main() {
         })
     }
 
+    for (const job of jobs) {
+        const data = new Date();
+
+        const br = new Date(data.setHours(data.getHours() - 3));
+
+        await prisma.jobs.create({
+            data: {...job, published: br, description: job.description},
+        })
+    }
+
     for (const usuario of usuarios) {
         const userc = {
             ...usuario, password: await bcrypt.hash(usuario.password, 10)
         }
-        if (userc.courseId !== undefined || userc.courseId !== null) {
             await prisma.user.create({
                 data: { ...userc, cnpj: userc.cnpj, courseId: userc.courseId, address: userc.address },
             })
-        }
     }
 }
 
