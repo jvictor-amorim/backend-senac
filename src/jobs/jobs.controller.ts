3140078689e3f72c7a0968777666f1ac094e28f5
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-04-17 10:40:47 -0300

    perf: ajustando roles para rotas das vagas

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -14,48 +14,66 @@ import { CreateUserJobDto } from './dto/create-user-job.dto';
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
-  // @ApiBearerAuth()
-  // @Roles(Role.ENTERPRISE)
-  // @UseGuards(JwtGuard, RolesGuard)
+  @ApiBearerAuth()
+  @Roles(Role.ENTERPRISE, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Post()
   create(@Body() createJobDto: CreateJobDto) {
     return this.jobsService.create(createJobDto);
   }
 
-  // @ApiBearerAuth()
-  // @Roles(Role.ENTERPRISE)
-  // @UseGuards(JwtGuard, RolesGuard)
+  @ApiBearerAuth()
+  @Roles(Role.USER, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Get()
   findAll() {
     return this.jobsService.findAll();
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.USER, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.jobsService.findOne(id);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.USER, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Get(':courseId')
   findByCourse(@Param('courseId') courseId: string) {
     return this.jobsService.findByCourse(courseId);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.ENTERPRISE, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
     return this.jobsService.update(+id, updateJobDto);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.ENTERPRISE, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.jobsService.remove(+id);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.USER)
+  @UseGuards(JwtGuard, RolesGuard)
   @Post('/create_user_job')
   create_user_job(@Body() userJobDto: CreateUserJobDto) {
     console.log(userJobDto)
     return this.jobsService.create_user_job(userJobDto);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.ENTERPRISE, Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Get('/job_user/:jobId')
   findByUserJob(@Param('jobId') jobId: string) {
     return this.jobsService.findByUserJob(jobId);
