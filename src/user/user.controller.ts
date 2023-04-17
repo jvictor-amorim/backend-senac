b8e09e2e54d90053ff8db4c23869d40d5ecc7201
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-04-17 10:43:47 -0300

    perf: ajustes nas roles do usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,6 +14,8 @@ import { UserService } from './user.service';
 import { CreateMailDto } from './dto/create-mail.dto';
 import { CreateSenacDto } from './dto/create-senac.dto';
 import { CreateEnterpriseDto } from './dto/create-enterprise.dto';
+import { UpdateEnterpriseDto } from './dto/update-enterprise.dto';
+import { UpdateSenacDto } from './dto/update-senac.dto';
 
 @ApiTags('Usuários')
 @Controller('users')
@@ -88,6 +90,22 @@ export class UserController {
   }
 
   @ApiBearerAuth()
+  @Roles(Role.ADMIN, Role.ENTERPRISE)
+  @UseGuards(JwtGuard, RolesGuard)
+  @Patch(':id')
+  updateEnterprise(@Param('id') id: string, @Body() updateUserDto: UpdateEnterpriseDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
+  @Patch(':id')
+  updateSenac(@Param('id') id: string, @Body() updateUserDto: UpdateSenacDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @ApiBearerAuth()
   @Roles(Role.ADMIN)
   @UseGuards(JwtGuard, RolesGuard)
   @Patch('secret/:id')
@@ -103,6 +121,9 @@ export class UserController {
     return this.userService.remove(id);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN, Role.SENAC)
+  @UseGuards(JwtGuard, RolesGuard)
   @Post('/mail')
   send_email(@Body() emailDto: CreateMailDto) {
     return this.userService.mail(emailDto);
