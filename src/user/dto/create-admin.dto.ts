c383b4020429acc37a280ee81d76358753600e86
Daniel Castro de Farias <45545000+danielcastrof@users.noreply.github.com>
2023-04-18 08:43:11 -0300

    feat: ajustes swagger

--- src/user/dto/create-admin.dto.ts
+++ src/user/dto/create-admin.dto.ts
@@ -1,13 +1,35 @@
 /* eslint-disable prettier/prettier */
 
+import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
-import {User} from '../entities/user.entity'
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { User } from '../entities/user.entity';
 
 export class CreateAdminDto extends User {
+  @ApiProperty({example: 'user@example.com'})
+  @IsEmail()
   email: string;
+
+  @ApiProperty({example: 'User001'})
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
+
+  @ApiProperty({example: 'João da Silva Sauros'})
+  @IsString()
   name: string;
+
+  @ApiProperty({example: '19909882712'})
+  @IsString()
   cpf: string;
+
+  @ApiProperty({example: '(81) 98765-4321'})
+  @IsString()
   phone: string;
+  
   role: Role;
 }
--- src/user/dto/create-senac.dto.ts
+++ src/user/dto/create-senac.dto.ts
@@ -1,11 +1,33 @@
 import { Role } from '@prisma/client';
 import {User} from '../entities/user.entity'
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateSenacDto extends User {
+  @ApiProperty({example: 'user@example.com'})
+  @IsEmail()
   email: string;
+
+  @ApiProperty({example: 'User001'})
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
+
+  @ApiProperty({example: 'João da Silva Sauros'})
+  @IsString()
   name: string;
+
+  @ApiProperty({example: '19909882712'})
+  @IsString()
   cpf: string;
+
+  @ApiProperty({example: '(81) 98765-4321'})
+  @IsString()
   phone: string;
+  
   role: Role;
 }
