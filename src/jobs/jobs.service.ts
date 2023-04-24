3f93d8db9ac64fc00d68aa2e48f6b4ed059cb9f0
Victor Amorim <victorsilva6075@gmail.com>
2023-04-23 21:38:17 -0300

    minor fix

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -91,6 +91,8 @@ export class JobsService {
 
   async findByName(courseName: string) {
     try { 
+      if(courseName == 'allvagas') return this.findAll()
+
       const find = await this.prisma.courses.findMany({
         where: {
           name: {contains: courseName, mode: 'insensitive'},
